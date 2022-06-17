f759e02c502ee44d5b9adbd0cb8a8077eced6910
yejin9337 <103083746+yejin9337@users.noreply.github.com>
2022-06-18 02:35:33 +0900

    commit
    textrpg

---
+++ 220618 textrpg/TextRPG/TextRPG/Player.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class player
+{
+public:
+
+private:
+};
---
+++ 220618 textrpg/TextRPG/TextRPG/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	printf("\n\n%15s\n\n%25s", "TEXT RPG","아무키나 눌러 시작\n\n");
+
+	getchar();
+	system("cls");
+
+	string userName;
+	cout << "사용할 이름을 입력해주세요\n\n";
+	cin >> userName;
+
+	system("cls");
+}
